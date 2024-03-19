0e691706797f60f055b17eefd6e66adb4ab86e3d
jorge perdomo <jorgamow@gmail.com>
2024-03-18 22:55:53 -0500

    Se agregó la pokeball al boton de pokedex en el menu de navegacion

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -2,28 +2,54 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import AccountScreen from "../screens/Account";
 import FavoriteScreen from "../screens/Favorite";
 import PokedexScreen from "../screens/Pokedex";
+import Icon from 'react-native-vector-icons/Ionicons';
+import { Image } from 'react-native';
+
 
 const Tab = createBottomTabNavigator();
 
-export default function Navigation(){
+export default function Navigation() {
 
   return (
     <Tab.Navigator>
-      <Tab.Screen 
-        name="Pokedex" 
+      <Tab.Screen
+        name="Favorite"
+        component={FavoriteScreen}
+        options={{
+          tabBarLabel: "Favoritos",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="heart" color={color} size={size} />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="Pokedex"
         component={PokedexScreen}
         options={{
-          tabBarLabel: "Pokedex"
+          tabBarLabel: "",
+          tabBarIcon: () => renderPokeball()
         }}
       />
-      <Tab.Screen 
-        name="Favorite" 
-        component={FavoriteScreen} 
+      <Tab.Screen
+        name="Account"
+        component={AccountScreen}
         options={{
-          tabBarLabel: "Favoritos"
+          tabBarLabel: "Mi cuenta",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="person" color={color} size={size} />
+          )
         }}
       />
-      <Tab.Screen name="Account" component={AccountScreen} />
     </Tab.Navigator>
+  );
+}
+
+
+function renderPokeball() {
+  return (
+    <Image
+      source={require('../assets/pokeball.png')}
+      style={{ width: 75, height: 75, top: -15 }}
+    />
   );
 }
