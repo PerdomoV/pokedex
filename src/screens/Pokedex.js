e3e824f1ea55b870850188f4f4b463fb9e13006c
Perdomov <jorgamow@gmail.com>
2024-04-01 21:26:18 -0500

    Se agregó una función para traer pokemones

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,6 +1,20 @@
+import React, {  useState, useEffect } from "react";
 import { SafeAreaView, Text } from "react-native";
+import { getPokemonsApi } from "../api/pokemon";
 
 export default function Pokedex(){
+
+  // const [pokemons, setPokemons] = useState({});
+
+  useEffect( () => {
+    console.log('En el useEffect');
+    getPokemonsApi().then(pokemones => {
+      console.log('pokemones: ', pokemones);
+    }).catch(error => {
+      console.error('Error en useEffect: ', error);
+    })
+  }, []);
+
   return (
     <SafeAreaView>
       <Text>Estamos en el screen de Pokedex.</Text>
