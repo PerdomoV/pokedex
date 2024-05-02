487649092a1a89609a7890ea3ac82e9b15fb030a
Jorge Andres Perdomo Velasquez <japerdomov@Jorges-Mac-mini.local>
2024-05-01 20:34:23 -0500

    Se repararon los estilos del pokemon card

--- src/components/Pokemon/Header.js
+++ src/components/Pokemon/Header.js
@@ -38,7 +38,7 @@ const styles = StyleSheet.create({
 
     bg: {
         width: "100%",
-        height: 800,
+        height: 400,
         position: "absolute",
         borderBottomEndRadius: 300,
         borderBottomLeftRadius: 300,
