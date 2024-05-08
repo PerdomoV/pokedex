d4b986c4b8b3b5a29364e78667ed662ab6500eca
Jorge Andres Perdomo Velasquez <japerdomov@Jorges-Mac-mini.local>
2024-05-07 20:10:56 -0500

    Se completó las estadisticas del pokemon

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import { ScrollView, Text } from "react-native";
 import { getPokemonDetailsApi } from "../api/pokemon";
 import Header from "../components/Pokemon/Header";
+import Type from "../components/Pokemon/Type";
+import Stats from "../components/Pokemon/Stats";
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 export default function Pokemon(props) {
 
@@ -15,6 +18,22 @@ export default function Pokemon(props) {
   console.log('id: ', params.id);
 
   const [pokemon, setPokemon ] = useState(null);
+
+  useEffect(()=>{
+    navigation.setOptions({
+      headerRight: () => null,
+      headerLeft: () => (
+        <Icon 
+          name="arrow-left" 
+          color="#fff" 
+          size={20} 
+          style={{marginLeft: 20, paddingBottom: 10}} 
+          onPress={()=>navigation.goBack()}
+        /> 
+      )
+    }),
+    [navigation, params]
+  })
 
   useEffect(() => {
     ( async () => {
@@ -37,6 +56,9 @@ export default function Pokemon(props) {
         image={pokemon.sprites.other["official-artwork"].front_default}
         type={pokemon.types[0].type.name}
       />
+
+    <Type types={pokemon.types}/>
+    <Stats stats={pokemon.stats}/>
     </ScrollView>
   );
 }
