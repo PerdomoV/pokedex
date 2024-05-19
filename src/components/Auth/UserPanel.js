59ff83a6d7d6d7288ffdb111deb30a140b950633
Jorge Andres Perdomo Velasquez <japerdomov@Jorges-Mac-mini.local>
2024-05-19 17:31:52 -0500

    Se agregó validación y manejo de estado al formulario de login con formik y yup

---
+++ src/components/Auth/UserPanel.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+export default function UserPanel(){
+  return (
+    <Text>Panel del usuario</Text>
+  );
+}
--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,9 +1,15 @@
 import { SafeAreaView, Text } from "react-native";
+import LoginForm from "../components/Auth/LoginForm";
+import UserPanel from "../components/Auth/UserPanel";
 
 export default function Account(){
+
+  let auth = null;
+
   return (
     <SafeAreaView>
-      <Text>Estamos en el screen de Account.</Text>
+      { auth ? <UserPanel/> : <LoginForm/> }
+      {/* <Text>Estamos en el screen de Account.</Text> */}
     </SafeAreaView>
   );
 }
